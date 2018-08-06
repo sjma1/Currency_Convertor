14dbb2f20ea3d103b8ffe4e9b3f8c3b80166064a
Seong Ma <sjma1@uci.edu>
2018-08-05 20:05:49 -0700

    Added "Currencies.txt" Added values to spinners using data from "Currencies.txt"

--- app/src/main/java/com/example/jun/currency_convertor/MainActivity.java
+++ app/src/main/java/com/example/jun/currency_convertor/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.jun.currency_convertor;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String currencies_filename = "Currencies.txt";
+
+        //Read "Currencies.txt" and use data to fill in Spinner values
+        try
+        {
+            ArrayList<String> currencies = this.readLines(currencies_filename);
+            Spinner c1 = (Spinner) findViewById(R.id.currency_1);
+            Spinner c2 = (Spinner) findViewById(R.id.currency_2);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item,
+                    currencies);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            c1.setAdapter(adapter);
+            c2.setAdapter(adapter);
+        }
+        catch(IOException e)
+        {
+            // Print out the exception that occurred
+            System.out.println("Unable to create "+ currencies_filename +": "+e.getMessage());
+        }
+
+    }
+
+    public ArrayList<String> readLines(String filename) throws IOException
+    {
+        FileReader fileReader = new FileReader(filename);
+
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ArrayList<String> lines = new ArrayList<String>();
+        String line = null;
+
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            lines.add(line);
+        }
+
+        bufferedReader.close();
+
+        return lines;
     }
 }
