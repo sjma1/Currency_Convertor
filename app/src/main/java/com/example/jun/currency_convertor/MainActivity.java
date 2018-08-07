f774e81405ea95355f03e5be16f62a652e6db851
Seong Ma <sjma1@uci.edu>
2018-08-07 13:59:25 -0700

    Created Spinner values and set their default values to USD and EUR

--- app/src/main/java/com/example/jun/currency_convertor/MainActivity.java
+++ app/src/main/java/com/example/jun/currency_convertor/MainActivity.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         String currencies_filename = "Currencies.txt";
 
+
         //Read "Currencies.txt" and use data to fill in Spinner values
         try
         {
             ArrayList<String> currencies = this.readLines(currencies_filename);
-            Spinner c1 = (Spinner) findViewById(R.id.currency_1);
-            Spinner c2 = (Spinner) findViewById(R.id.currency_2);
+            Spinner c1_spinner = (Spinner) findViewById(R.id.currency_1);
+            Spinner c2_spinner = (Spinner) findViewById(R.id.currency_2);
 
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_spinner_item,
@@ -34,22 +39,26 @@ public class MainActivity extends AppCompatActivity {
 
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-            c1.setAdapter(adapter);
-            c2.setAdapter(adapter);
+            //Give spinner adapter values and set their default positions to USD and EUR
+            c1_spinner.setAdapter(adapter);
+            c2_spinner.setAdapter(adapter);
+            c1_spinner.setSelection(adapter.getPosition("USD \tUnited States Dollar"));
+            c2_spinner.setSelection(adapter.getPosition("EUR \tEuro"));
         }
         catch(IOException e)
         {
             // Print out the exception that occurred
             System.out.println("Unable to create "+ currencies_filename +": "+e.getMessage());
+            Toast.makeText(MainActivity.this, "FAILED", Toast.LENGTH_SHORT).show();
         }
 
     }
 
     public ArrayList<String> readLines(String filename) throws IOException
     {
-        FileReader fileReader = new FileReader(filename);
+        final InputStream currencies_file = getAssets().open(filename);
 
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(currencies_file));
         ArrayList<String> lines = new ArrayList<String>();
         String line = null;
 
